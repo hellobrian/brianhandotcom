d828fa7c72adb567ea43ab9f3017e7d5233f9b5f
hellobrian <brianhan@hey.com>
2024-03-02 17:53:03 -0600

    first commit

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      // heroImage: z.string().optional(),
+      cover: image().refine((img) => img.width >= 768, {
+        message: "Cover image must be at least 768 pixels wide!",
+      }),
+      coverAlt: z.string(),
+    }),
+});
+
+export const collections = { blog };
